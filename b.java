50e3886014f6221b0b798d70aedad805d8115ded
Nikhil257v <nikhilvarma257@gmail.com>
2024-10-16 11:06:12 +0530

    Add files via upload

---
+++ b.java
@@ -0,0 +1,18 @@
+public class MathExample {
+public static void main(String[] args) {
+ Calculate the circumference of a circle with a radius of 5
+double radius = 5;
+double circumference = 2  Math.PI  radius;
+System.out.println(Circumference  + circumference);
+
+ Calculate the logarithm of 1000
+double asdfghjklkjkhbbnm,m,nbvm bvc
+sdfgmnm,k
+cgbnm.logValue = Math.log10(1000);
+System.out.println(Logarithm of 1000  + logValue);
+
+ Generate a random number between 0 and 1
+double randomValue = Math.random();
+System.out.println(Random number  + randomValue);
+}
+}
